2312981d997a67803b65bfe8e6c225b70504a531
Maithili-Badhan <maithilibprojects@gmail.com>
2025-07-27 13:11:21 +0530

    Data Ingestion initial

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -133,4 +133,4 @@ def decodeImage(imgstring, fileName):
 
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
-        return base64.b64encode(f.read())open
+        return base64.b64encode(f.read())
